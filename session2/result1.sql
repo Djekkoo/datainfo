32f57f28ae39f766f954f5bfd152404f0f5e9b77
Djekkoo <djekkoo@hotmail.com>
2014-05-06 15:37:41 +0200

    Added session2

---
+++ session2/result1.sql
@@ -0,0 +1,14 @@
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES
+('a', 0, 'Meerdere voertuigen kunnen dezelfde capaciteit hebben'),
+('b', 1, 'Een voertuig heeft maar één capaciteit'),
+('c', 0, 'een afleveradres en een factuur kunnen naar verschillende pakketjes verwijzen'),
+('d', 1, 'Een pakketje wordt maar op één factuur vermeld en maar op één adres bezorgt'),
+('e', 1, 'van elk pakketje is de prijs en de ontvanger bekend'),
+('f', 0, 'dezelfde persoon kan verschillende voertuigen gebruiken voor verschillende pakkejes'),
+('g', 0, 'verschillende personen kunnen hetzelfde voertuig gebruikt hebben'),
+('h', 0, 'Niet aan toegekomen'),
+('i', 0, 'Niet aan toegekomen'),
+('j', 0, 'Niet aan toegekomen');
+
+ 
