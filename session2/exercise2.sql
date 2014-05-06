6834368696e1535ded2e58d03ba369671ba03394
Djekkoo <djekkoo@hotmail.com>
2014-05-06 16:09:18 +0200

    Added task 2.2 C

--- session2/exercise2.sql
+++ session2/exercise2.sql
@@ -8,3 +8,13 @@ CREATE TABLE R2(D INT, A INT);
 
 CREATE TABLE F1(x VARCHAR(10), y VARCHAR(10), violation BOOLEAN, motivation VARCHAR(200));
 CREATE TABLE F2(x VARCHAR(10), y VARCHAR(10), violation BOOLEAN, motivation VARCHAR(200));
+
+
+-- D ook gescheiden uit de tabel, met de koppeling E->D
+CREATE TABLE Rn1(B INT, C INT, E INT, F INT);
+CREATE TABLE Rn2(D INT, A INT);
+CREATE TABLE Rn3(E INT, D INT);
+
+CREATE TABLE Fn1(x VARCHAR(10), y VARCHAR(10), violation BOOLEAN, motivation VARCHAR(200));
+CREATE TABLE Fn2(x VARCHAR(10), y VARCHAR(10), violation BOOLEAN, motivation VARCHAR(200));
+CREATE TABLE Fn3(x VARCHAR(10), y VARCHAR(10), violation BOOLEAN, motivation VARCHAR(200));
--- session2/result2.sql
+++ session2/result2.sql
@@ -3,9 +3,20 @@ INSERT INTO F (x, y, violation, motivation) VALUES
 ("E", "D", 1, "With only ED known, D->A gives ADE known. There is no other way to 'learn' more keys"),
 ("D", "A", 1, "With only AD known, there is no other key that can be learned"); 
 
+--B
 INSERT INTO F1 (x, y, violation, motivation) VALUES
 ("BC", "EF", 0, "E->D provides all attributes"),
 ("E", "D", 1, "DE gives no opportunity to learn other attributes");
 
 INSERT INTO F2 (x, y, violation, motivation) VALUES
 ("D", "A", 0, "That are the only values");
+
+--C
+INSERT INTO Fn1 (x, y, violation, motivation) VALUES
+("BC", "EF", 0, "This is the only dependency");
+
+INSERT INTO Fn2 (x, y, violation, motivation) VALUES
+("D", "A", 0, "This is the only dependency");
+
+INSERT INTO Fn3 (x, y, violation, motivation) VALUES
+("E", "D", 0, "This is the only dependency");
