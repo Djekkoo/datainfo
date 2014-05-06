f81a02c5cf50cb6f30313a83ac9e6437dfe9928e
Djekkoo <djekkoo@hotmail.com>
2014-05-06 15:55:26 +0200

    add task 2.2 a

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,1 +1,4 @@
- 
+INSERT INTO F (x, y, violation, motivation) VALUES
+("ABC", "EF", 0, "Not a violation, E->D means that all attributes are known"),
+("E", "D", 1, "With only ED known, D->A gives ADE known. There is no other way to 'learn' more keys"),
+("D", "A", 1, "With only AD known, there is no other key that can be learned"); 
